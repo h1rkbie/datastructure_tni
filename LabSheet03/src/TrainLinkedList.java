954c915df526933a624da588498bf7e11b7885bb
Hark <sxghsun@gmail.com>
2026-07-20 10:19:52 +0700

    2513110060 Submit LabSheet03

---
+++ LabSheet03/src/TrainLinkedList.java
@@ -0,0 +1,34 @@
+public class TrainLinkedList {
+    static void main(String[] args)
+    {
+        LinkedList greenline = new LinkedList();
+        greenline.insert(0, "Mo-chit");
+        greenline.insert(1, "Ari");
+        greenline.insert(2, "Sanam-Pao");
+
+        System.out.println("All Stations : " + greenline.traversal());
+
+        greenline.insert(2,"Rachakru");
+        System.out.println("All Stations : " + greenline.traversal());
+
+        System.out.println("Total Station : " + greenline.length());
+
+        System.out.println("First Station : " + greenline.get(0));
+
+        greenline.set(0, "Mo-Chit (Central Ladprao)");
+        System.out.println("All Stations : " + greenline.traversal());
+
+        greenline.remove(2);
+        System.out.println("All Stations : " + greenline.traversal());
+
+        greenline.removeLastElement();
+        System.out.println("All Stations : " + greenline.traversal());
+
+        greenline.clear();
+        System.out.println("Total Stations : " + greenline.length());
+
+        System.out.println("All Stations : " + greenline.traversal());
+    }
+
+
+}
