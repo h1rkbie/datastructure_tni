8c57ff244b73035d61486851b0300ad05f2a91e6
Hark <sxghsun@gmail.com>
2026-07-25 22:04:23 +0700

    2513110060 Submit LabSheet04

---
+++ LabSheet04/src/TodoList.java
@@ -0,0 +1,33 @@
+import java.sql.SQLOutput;
+
+public class TodoList
+{
+    public static void main(String[] arg)
+    {
+        DoublyLinkedList todoList = new DoublyLinkedList();
+        todoList.insert("Finish Homework");
+        todoList.insert("Laundry");
+        todoList.insert("Group Meeting");
+
+        System.out.println("To-Do List = " + todoList.traversal());
+
+        todoList.insert(0, "Submit Report");
+        System.out.println("To-Do List = " + todoList.traversal());
+
+        todoList.insert(2, "Buy Food");
+        System.out.println("To-Do List = " + todoList.traversal());
+
+        todoList.insert("Go to Gym");
+        System.out.println("To-Do List = " + todoList.traversal());
+
+        todoList.remove();
+        todoList.remove(3);
+        todoList.remove(0);
+        System.out.println("To-Do List = " + todoList.traversal());
+
+        System.out.println("\nBackward Traversal = " + todoList.backwardTraversal());
+
+
+    }
+
+}
